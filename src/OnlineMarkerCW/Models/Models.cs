1df5ad5750966e4a36b9ca2ebd8a9f5e7a89ccd0
AndresMartinMaq <andrewtri800@hotmail.com>
2016-10-14 13:23:34 +0100

    Added a model for teacher

---
+++ src/OnlineMarkerCW/Models/Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarkerCW.Models{
+
+    public class Teacher{
+
+        public int id;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+
+    }
+}
