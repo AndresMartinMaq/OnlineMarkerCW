29d3a8b44abdf92cf5ce9d3751b5a36ed27476cf
AndresMartinMaq <andrewtri800@hotmail.com>
2016-11-17 13:09:45 +0000

    Did some config so that it would accept the tests project.

--- src/OnlineMarkerCW/wwwroot/_references.js
+++ src/OnlineMarkerCW/wwwroot/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="fonts/fonts/galano_grotesque_bold/Webfonts/galanogrotesquedemo_bold_macroman/specimen_files/easytabs.js" />
+/// <reference path="js/code-html-view.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/bootstrap/dist/js/npm.js" />
