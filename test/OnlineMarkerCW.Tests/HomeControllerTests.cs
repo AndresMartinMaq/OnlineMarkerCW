90dc14fbccacaad7e81733dae01c7d4d4911035a
AndresMartinMaq <andrewtri800@hotmail.com>
2016-11-21 15:38:13 +0000

    Tried to mock users, fails. Added a constructor for home controller without logger, just because otherwise it crashed on testing and I dont know how to create

--- test/OnlineMarkerCW.Tests/HomeControllerTests.cs
+++ test/OnlineMarkerCW.Tests/HomeControllerTests.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using OnlineMarkerCW.Models;
+using System.Threading.Tasks;
+using OnlineMarkerCW.Controllers;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
 
 namespace OnlineMarkerCW.UnitTests.Controllers
 {
@@ -31,5 +36,57 @@ namespace OnlineMarkerCW.UnitTests.Controllers
 
              Assert.False(result, $"{value} should not be prime");
          }
+
+        //Get a user instance
+        private ApplicationUser generateStu() {
+            ApplicationUser u = new ApplicationUser();
+            u.Name = "Stuart";
+            u.Surname = "Studentsen";
+            return u;
+        }
+
+        [Fact]
+        public async Task IndexTest1()
+        {
+            // --Arrange--
+            var controller = new HomeController(null, new LoggerFactory(), null, null, null);
+            /*var mockClaims = new Mock<System.Security.Claims.ClaimsPrincipal>();
+            mockClaims.Setup(userClaims => userClaims.FindFirstValue(ClaimTypes.Role)).Returns("User");
+            controller.User = mockClaims; //Controller.User cannot be set directly as here.*/
+
+            //Create fake student identity (User).
+            //Method 1
+            GenericIdentity fakeIdentity = new GenericIdentity("SomeUser");
+            fakeIdentity.AddClaim(new Claim(ClaimTypes.Role, "Student"));
+            GenericPrincipal principal = new GenericPrincipal(fakeIdentity, null);
+            //Mock Context to include the fake user
+            /*var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.Setup(t => t.User).Returns(principal);
+            var controllerContext = new Mock<ControllerContext>();
+            controllerContext.Setup(t => t.HttpContext).Returns(mockHttpContext.Object);*/
+            //Set home controller to use mock context
+            //controller.ControllerContext = controllerContext.Object;
+
+            //Method 2
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, "1"),
+                 new Claim(ClaimTypes.Role, "Student")
+            }));
+            //Set context
+            controller.ControllerContext = new ControllerContext() {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Home", redirectToActionResult.ControllerName);
+            Assert.Equal("MyWorks", redirectToActionResult.ActionName);     //MyWorks if student, MyMarkings if Teacher.
+        }
     }
 }
+
