720bbf116d05c0c24eacc25c67ab5ab42c58544e
AndresMartinMaq <andrewtri800@hotmail.com>
2016-11-27 13:28:49 +0000

    tried more, no success

--- test/OnlineMarkerCW.Tests/HomeControllerTests.cs
+++ test/OnlineMarkerCW.Tests/HomeControllerTests.cs
@@ -12,6 +12,8 @@ using OnlineMarkerCW.Controllers;
 using System.Security.Claims;
 using System.Security.Principal;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace OnlineMarkerCW.UnitTests.Controllers
 {
@@ -49,25 +51,48 @@ namespace OnlineMarkerCW.UnitTests.Controllers
         public async Task IndexTest1()
         {
             // --Arrange--
+            //-Controller
             var controller = new HomeController(null, new LoggerFactory(), null, null, null);
-            /*var mockClaims = new Mock<System.Security.Claims.ClaimsPrincipal>();
-            mockClaims.Setup(userClaims => userClaims.FindFirstValue(ClaimTypes.Role)).Returns("User");
-            controller.User = mockClaims; //Controller.User cannot be set directly as here.*/
+            //-User
+            var m_userClaims = new Mock<System.Security.Claims.ClaimsPrincipal>();
+            var m_Identity = new Mock<IIdentity>();
+            m_Identity.Setup(identity => identity.IsAuthenticated).Returns(true);
+            m_userClaims.Setup(u => u.Identity).Returns(m_Identity.Object);
+            m_userClaims.Setup(u => u.IsInRole("Student")).Returns(true);
+            //m_userClaims.Setup(u => u.FindFirstValue(ClaimTypes.Role)).Returns("Student"); //doesnt work
+            //-httpContext
+            var m_HtttpContext = new DefaultHttpContext() { User = m_userClaims.Object };
 
+            //var controllerContext = new ControllerContext(m_HtttpContext, new RouteData(), new ControllerBase());
+            var controllerContext = new ControllerContext(new ActionContext(
+                m_HtttpContext, 
+                new RouteData(), 
+                new Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor(), 
+                new ModelStateDictionary()));
+            //controllerContext.Setup(t => t.HttpContext).Returns(m_HtttpContext);
+             //Set home controller to use mock context
+             controller.ControllerContext = controllerContext;
+
+             //Create ActionExecutingContext.
+             /*var actionContext = new ActionContext(m_HtttpContext, new RouteData(), new ActionDescriptor(), new ModelStateDictionary());
+            var ListFilterMetaData = new List<IFilterMetadata>() { new Mock<IFilterMetadata>().Object };
+            var actionExecutingContext = new ActionExecutingContext(actionContext, ListFilterMetaData, new Dictionary<string, object>(), null);
+            //Initiate the filter*/
+            
             //Create fake student identity (User).
             //Method 1
-            GenericIdentity fakeIdentity = new GenericIdentity("SomeUser");
+            /*GenericIdentity fakeIdentity = new GenericIdentity("SomeUser");
             fakeIdentity.AddClaim(new Claim(ClaimTypes.Role, "Student"));
             GenericPrincipal principal = new GenericPrincipal(fakeIdentity, null);
             //Mock Context to include the fake user
-            /*var mockHttpContext = new Mock<HttpContext>();
+            var mockHttpContext = new Mock<HttpContext>();
             mockHttpContext.Setup(t => t.User).Returns(principal);
             var controllerContext = new Mock<ControllerContext>();
             controllerContext.Setup(t => t.HttpContext).Returns(mockHttpContext.Object);*/
             //Set home controller to use mock context
             //controller.ControllerContext = controllerContext.Object;
 
-            //Method 2
+            /*//Method 2
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                  new Claim(ClaimTypes.Name, "1"),
@@ -76,7 +101,7 @@ namespace OnlineMarkerCW.UnitTests.Controllers
             //Set context
             controller.ControllerContext = new ControllerContext() {
                 HttpContext = new DefaultHttpContext() { User = user }
-            };
+            };*/
 
 
             // Act
